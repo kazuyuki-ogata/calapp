fd8c16b54100ec3c9dd0345f54040d0a7a8b7e04
kazuyuki-ogata <ogata@itto.co.jp>
2018-03-24 20:34:28 +0900

    ナビバーの作成

--- src/main/java/javax/activation/calapp/app/CalendarController.java
+++ src/main/java/javax/activation/calapp/app/CalendarController.java
@@ -1,8 +1,5 @@
 package javax.activation.calapp.app;
 
-import javax.activation.calapp.domain.service.CalService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("cal")
 public class CalendarController {
-
-	@Autowired
-	CalService calService;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String show(Model model) {
--- src/main/resources/static/js/cal/cal.js
+++ src/main/resources/static/js/cal/cal.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+	moment.locale("ja");
+	setInterval(function(){
+		var currentTime = moment().format("YYYY年MM月DD日（ddd） HH:mm:ss ");
+		$('#currentTime').html(currentTime);
+	}, 1000);
+
 	$.ajax({
 		type : "GET",
 		url : "http://localhost:8080/api/events",
@@ -21,5 +27,34 @@ $(document).ready(function() {
 		});
 	});
 
-	// $('#top').
+	$('#reload').click(function() {
+		location.reload();
+	});
+
+	$('#bus').click(function(event) {
+		event.preventDefault();
+		$('#modal-default').iziModal('open');
+	});
+	$('#modal-default').iziModal({
+		headerColor : '#26A69A',
+		width : 700,
+	});
+
+	$('#event1').click(function(event) {
+		event.preventDefault();
+		$('#modal-event1').iziModal('open');
+	});
+	$('#modal-event1').iziModal({
+		headerColor : '#26A69A',
+		width : 800,
+	});
+
+	$('#event2').click(function(event) {
+		event.preventDefault();
+		$('#modal-event2').iziModal('open');
+	});
+	$('#modal-event2').iziModal({
+		headerColor : '#26A69A',
+		width : 800,
+	});
 });
